f33df9ffe4b92df1ff329988d7e14fc6e7e4acf4
dobby-zent <dobby.lee@zent.kr>
2024-09-08 20:06:37 +0900

    init ssm

---
+++ internal/ssm.go
@@ -0,0 +1,19 @@
+package internal
+
+const SSMKEY = "/ddl/info/secrets"
+
+func (p *AWSClientParams) InitSSM() {
+
+}
+
+func (p *AWSClientParams) GetSSMValue() {
+
+}
+
+func (p *AWSClientParams) UpdateSSMValue() {
+
+}
+
+func (p *AWSClientParams) DeleteSSMValue() {
+
+}
---
+++ internal/ssm_test.go
@@ -0,0 +1,7 @@
+package internal
+
+import "testing"
+
+func Test_InitSSM(t *testing.T) {
+
+}
